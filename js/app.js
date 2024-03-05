88853a25482864bacab8e18d74f3cc440cf79baf
soheilMansoori <ahadmansoori369@gmail.com>
2024-03-05 15:12:56 +0330

    create header and change navbar name

--- js/app.js
+++ js/app.js
@@ -1,76 +1,63 @@
 console.log('hi')
-    // <section class="pt-20 mb-10 px-4" >
-    //     <div class="container mx-auto max-w-screen-xl">
-    //         <div class="flex justify-center relative mb-14">
-    //             <h2 class="font-YekanBakh-ExtraBlack text-3xl">پـــــــــــــروژه لاریـــــن</h2>
-    //             <div class="absolute -top-6">
-    //                 <span class="font-YekanBakh-ExtraBlack text-6xl text-opacity-10 text-stone-900">projects</span>
 
-    //             </div>
-    //             <div class="bg-orange-200 w-20 h-1.5 rounded-full absolute top-10"></div>
-
-    //         </div>
-    //         <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
-    //             <!-- image - start -->
-    //             <a href="single-project.html"
-    //                 class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-72">
-    //                 <img src="../assets/images/project-30.jpg" loading="lazy" alt="Photo by Minh Pham"
-    //                     class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
-
-    //                 <div
-    //                     class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
-    //                 </div>
-
-    //                 <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
-    //                     یک</span>
-    //             </a>
-    //             <!-- image - end -->
-
-    //             <!-- image - start -->
-    //             <a href="single-project.html"
-    //                 class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-72">
-    //                 <img src="../assets/images/blog-2.jpg" loading="lazy" alt="Photo by Magicle"
-    //                     class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
-
-    //                 <div
-    //                     class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
-    //                 </div>
-
-    //                 <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
-    //                     دو</span>
-    //             </a>
-    //             <!-- image - end -->
-
-    //             <!-- image - start -->
-    //             <a href="single-project.html"
-    //                 class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-72">
-    //                 <img src="../assets/images/project-31.jpg" loading="lazy" alt="Photo by Martin Sanchez"
-    //                     class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
-
-    //                 <div
-    //                     class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
-    //                 </div>
-
-    //                 <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
-    //                     سه</span>
-    //             </a>
-    //             <!-- image - end -->
-
-    //             <!-- image - start -->
-    //             <a href="single-project.html"
-    //                 class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-72">
-    //                 <img src="../assets/images/blog-4.jpg"
-    //                     class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
-
-    //                 <div
-    //                     class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
-    //                 </div>
-
-    //                 <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
-    //                     4</span>
-    //             </a>
-    //             <!-- image - end -->
-    //         </div>
-
-    //     </div>
-    // </section >
+{/* <section class="pt-20 mb-10 px-4">
+<div class="container mx-auto max-w-screen-xl">
+    <div class="flex justify-center relative mb-14">
+        <h2 class="font-YekanBakh-ExtraBlack text-3xl">پـــــــــــــروژه لاریـــــن</h2>
+        <div class="absolute -top-6">
+            <span class="font-YekanBakh-ExtraBlack text-6xl text-opacity-10 text-stone-900">projects</span>
+        </div>
+        <div class="bg-orange-200 w-20 h-1.5 rounded-full absolute top-10"></div>
+    </div>
+    <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        <!-- image - start -->
+        <a href="single-project.html"
+            class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-72">
+            <img src="../assets/images/project-30.jpg" loading="lazy" alt="Photo by Minh Pham"
+                class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
+            <div
+                class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+            </div>
+            <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
+                یک</span>
+        </a>
+        <!-- image - end -->
+        <!-- image - start -->
+        <a href="single-project.html"
+            class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-72">
+            <img src="../assets/images/blog-2.jpg" loading="lazy" alt="Photo by Magicle"
+                class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
+            <div
+                class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+            </div>
+            <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
+                دو</span>
+        </a>
+        <!-- image - end -->
+        <!-- image - start -->
+        <a href="single-project.html"
+            class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-72">
+            <img src="../assets/images/project-31.jpg" loading="lazy" alt="Photo by Martin Sanchez"
+                class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
+            <div
+                class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+            </div>
+            <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
+                سه</span>
+        </a>
+        <!-- image - end -->
+        <!-- image - start -->
+        <a href="single-project.html"
+            class="group relative flex justify-center h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-72">
+            <img src="../assets/images/blog-4.jpg"
+                class="absolute inset-0 h-full w-full object-cover object-center transition duration-300 group-hover:scale-110" />
+            <div
+                class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+            </div>
+            <span class="relative mb-4 flex justify-center text-base text-white text-center">پروژه شماره
+                4</span>
+        </a>
+        <!-- image - end -->
+    </div>
+</div>
+</section> */}
